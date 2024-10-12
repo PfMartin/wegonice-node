a5efdf7888a0ace7540801493efdca60871f0e15
PfMartin <martinpfatrisch@gmail.com>
2024-10-12 07:17:08 +0200

    Finished test for config

--- api/config/config.ts
+++ api/config/config.ts
@@ -9,27 +9,27 @@ export const getConfig = (path: string) => {
         process.env.WEGONICE_DB ||
         (() => {
           throw new Error('WEGONICE_DB variable is not set');
-        }),
+        })(),
       dbUser:
         process.env.WEGONICE_USER ||
         (() => {
           throw new Error('WEGONICE_USER variable is not set');
-        }),
+        })(),
       dbUserPwd:
         process.env.WEGONICE_PWD ||
         (() => {
           throw new Error('WEGONICE_PWD variable is not set');
-        }),
+        })(),
       dbHost:
-        process.env.WEGONCIE_HOST ||
+        process.env.WEGONICE_HOST ||
         (() => {
           throw new Error('WEGONICE_HOST variable is not set');
-        }),
+        })(),
       dbPort:
         process.env.WEGONICE_PORT ||
         (() => {
           throw new Error('WEGONICE_PORT variable is not set');
-        }),
+        })(),
     };
     return { data, error: '' };
   } catch (err: unknown) {
--- tests/config.test.ts
+++ tests/config.test.ts
@@ -1,14 +1,31 @@
 import { getConfig } from '../api/config/config';
 
 describe('config', () => {
+  const originalEnv = { ...process.env };
+
+  beforeEach(() => {
+    process.env = { ...originalEnv };
+  });
+
+  afterEach(() => {
+    jest.resetModules(); // This clears the module cache
+  });
+
   it('gets the correct config', () => {
-    const { data, error } = getConfig('./.env');
+    const { data, error } = getConfig('./tests/.env');
 
     expect(error).toEqual('');
-    expect(data?.dbName).toBeTruthy();
-    expect(data?.dbUser).toBeTruthy();
-    expect(data?.dbUserPwd).toBeTruthy();
-    expect(data?.dbHost).toBeTruthy();
-    expect(data?.dbPort).toBeTruthy();
+    expect(data?.dbName).toBe('wegonice');
+    expect(data?.dbUser).toBe('niceUser');
+    expect(data?.dbUserPwd).toBe('nicePassword');
+    expect(data?.dbHost).toBe('0.0.0.0');
+    expect(data?.dbPort).toBe('27017');
+  });
+
+  it('fails to get the config due to missing port variable', () => {
+    const { data, error } = getConfig('./tests/.test.env');
+
+    expect(error).toBeTruthy();
+    expect(data).toBeNull();
   });
 });
